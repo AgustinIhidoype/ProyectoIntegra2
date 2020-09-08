bb51babd836a6c260eb4b90ab9937789c6d12774
Lucas Groszmann <lgroszmann@udesa.edu.ar>
2020-09-08 17:56:00 -0300

    ruta home 1

--- Controllers/homeController.js
+++ Controllers/homeController.js
@@ -1,5 +1,7 @@
 let homeController = {
-
+hola: function (req, res) {
+    res.send ("hola")
+}
 }
 
 module.exports= homeController
--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 var homeController = require ("../Controllers/homeController");
 
-router.get ("/home" , homeController)
+router.get ("/home" , homeController.hola)
 
 module.exports = router;
