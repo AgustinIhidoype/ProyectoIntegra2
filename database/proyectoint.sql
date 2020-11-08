04532fa0fac9b0ac4b9fcb73f7230c76b3bcb93f
Lucas Groszmann <lgroszmann@udesa.edu.ar>
2020-11-08 15:08:38 -0300

    asd

--- database/proyectoint.sql
+++ database/proyectoint.sql
@@ -1,13 +1,13 @@
--- MariaDB dump 10.17  Distrib 10.4.14-MariaDB, for Win64 (AMD64)
+-- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
 --
 -- Host: 127.0.0.1    Database: proyectoint
 -- ------------------------------------------------------
--- Server version	10.4.14-MariaDB
+-- Server version	5.7.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `comentarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `comentarios` (
   `id` int(13) NOT NULL AUTO_INCREMENT,
   `id_post` int(11) NOT NULL,
@@ -51,13 +51,13 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `posts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_usuario` int(12) NOT NULL,
   `url_imagen` varchar(400) DEFAULT NULL,
   `texto` varchar(45) DEFAULT NULL,
-  `fecha_publicación` timestamp NULL DEFAULT current_timestamp(),
+  `fecha_publicación` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `id_usuario` (`id_usuario`),
   CONSTRAINT `id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON UPDATE NO ACTION
@@ -80,11 +80,11 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `usuarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios` (
   `id` int(12) NOT NULL AUTO_INCREMENT,
   `nombre_usuario` varchar(45) DEFAULT NULL,
-  `fecha_registracion` timestamp NULL DEFAULT current_timestamp(),
+  `fecha_registracion` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `email` varchar(45) DEFAULT NULL,
   `constrasenia` varchar(200) DEFAULT NULL,
   `fecha_nacimiento` datetime DEFAULT NULL,
@@ -93,7 +93,7 @@ CREATE TABLE `usuarios` (
   `fotoPerfil` varchar(250) NOT NULL DEFAULT 'sinfoto.jpg',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,13 +102,9 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` VALUES (1,'BoJack','2015-05-23 00:00:00','bojack@gmail.com','paredes','1996-02-28 00:00:00',NULL,NULL,'fotoperfilbojack.jpg'),(2,'Pluzito','2019-12-04 00:00:00','pluzito@gmail.com','siluetas','1990-04-05 00:00:00',NULL,NULL,'lafotodepluzito.jpg'),(3,'Bob Esponja','2015-07-24 00:00:00','bob@gmail.com','esponjamojada','1978-07-06 00:00:00',NULL,NULL,'fotoperfilbobesponja.jpg'),(4,'Goku','2014-09-11 00:00:00','goku@gmail.com','fuego123','1823-11-13 00:00:00',NULL,NULL,'goku.jpg'),(5,'Morty','2015-02-27 00:00:00','morty@gmail.com','yugoslavia','1998-12-12 00:00:00',NULL,NULL,'morty.jpg'),(6,'Rick','2016-01-02 00:00:00','rick@gmail.com','palangana130','1995-01-01 00:00:00',NULL,NULL,'ricky.jpg'),(7,'Juan','2020-10-19 16:50:06','juan@gmail.com','juan','1990-05-05 00:00:00',NULL,NULL,'sinfoto.jpg'),(14,'aguslerej','2020-10-26 14:31:12','aguslerej@gmail.com','$2a$10$uzbx4gjOOSwBQPphSCP7YulfO8nxRWeBPpcbrG','2020-12-01 00:00:00',1,'verde','sinfoto.jpg'),(15,'asd','2020-10-26 14:32:22','asd@gmail.com','$2a$10$9OES7NVufBC7PsJonHTPEOyk6SJRpjOyXs7p1k','2019-11-30 00:00:00',2,'vietnam','sinfoto.jpg'),(16,'asd1','2020-10-26 15:37:20','asd1@gmail.com','$2a$10$VFtB4kDloHJ7i9.QN5mISOShqpCv7PRno3sA4G','2014-11-30 00:00:00',1,'asd','sinfoto.jpg'),(17,'agustin','2020-10-26 15:48:00','agustin@gmail.com','$2a$10$yJWi8Mqa/M3QtQmGyK7.tOb3X1eQsGVcpscJAaOwOCmxx6qWSrnBK','2013-10-31 00:00:00',1,'asd','sinfoto.jpg'),(18,'Juan Cruz','2020-10-26 16:09:55','juancruz@gmail.com','$2a$10$oh.7rozDnsHzGcoLh6eZxuQw39T9uIinoZyqCjIeb7UtvxDusCMOq','2111-04-24 00:00:00',1,'verde','sinfoto.jpg'),(19,'Lucas','2020-11-02 14:05:20','lucasgroszmann@yahoo.com.ar','$2a$10$BsqpXJqEcUUCVhPqfuZVwuLq87Q2KkkDOmaGeubcyrXaCR/Mjjs4.','1999-04-20 00:00:00',1,'azul','sinfoto.jpg');
+INSERT INTO `usuarios` VALUES (1,'BoJack','2015-05-23 00:00:00','bojack@gmail.com','paredes','1996-02-28 00:00:00',NULL,NULL,'fotoperfilbojack.jpg'),(2,'Pluzito','2019-12-04 00:00:00','pluzito@gmail.com','siluetas','1990-04-05 00:00:00',NULL,NULL,'lafotodepluzito.jpg'),(3,'Bob Esponja','2015-07-24 00:00:00','bob@gmail.com','esponjamojada','1978-07-06 00:00:00',NULL,NULL,'fotoperfilbobesponja.jpg'),(4,'Goku','2014-09-11 00:00:00','goku@gmail.com','fuego123','1823-11-13 00:00:00',NULL,NULL,'goku.jpg'),(5,'Morty','2015-02-27 00:00:00','morty@gmail.com','yugoslavia','1998-12-12 00:00:00',NULL,NULL,'morty.jpg'),(6,'Rick','2016-01-02 00:00:00','rick@gmail.com','palangana130','1995-01-01 00:00:00',NULL,NULL,'ricky.jpg'),(7,'Juan','2020-10-19 16:50:06','juan@gmail.com','juan','1990-05-05 00:00:00',NULL,NULL,'sinfoto.jpg'),(14,'aguslerej','2020-10-26 14:31:12','aguslerej@gmail.com','$2a$10$uzbx4gjOOSwBQPphSCP7YulfO8nxRWeBPpcbrG','2020-12-01 00:00:00',1,'verde','sinfoto.jpg'),(15,'asd','2020-10-26 14:32:22','asd@gmail.com','$2a$10$9OES7NVufBC7PsJonHTPEOyk6SJRpjOyXs7p1k','2019-11-30 00:00:00',2,'vietnam','sinfoto.jpg'),(16,'asd1','2020-10-26 15:37:20','asd1@gmail.com','$2a$10$VFtB4kDloHJ7i9.QN5mISOShqpCv7PRno3sA4G','2014-11-30 00:00:00',1,'asd','sinfoto.jpg'),(17,'agustin','2020-10-26 15:48:00','agustin@gmail.com','$2a$10$yJWi8Mqa/M3QtQmGyK7.tOb3X1eQsGVcpscJAaOwOCmxx6qWSrnBK','2013-10-31 00:00:00',1,'asd','sinfoto.jpg'),(18,'Juan Cruz','2020-10-26 16:09:55','juancruz@gmail.com','$2a$10$oh.7rozDnsHzGcoLh6eZxuQw39T9uIinoZyqCjIeb7UtvxDusCMOq','2111-04-24 00:00:00',1,'verde','sinfoto.jpg'),(19,'Lucas','2020-11-02 14:05:20','lucasgroszmann@yahoo.com.ar','$2a$10$BsqpXJqEcUUCVhPqfuZVwuLq87Q2KkkDOmaGeubcyrXaCR/Mjjs4.','1999-04-20 00:00:00',1,'azul','sinfoto.jpg'),(20,'pedro martinez','2020-11-08 17:50:01','lucasgroszmann@gmail.com','$2a$10$Rv.MZPazek5EsUVq9RExJ.P/RGxqodTih9.RB1I7qr2PRIuvh2kJ6','2000-04-20 00:00:00',1,'azul','sinfoto.jpg');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'proyectoint'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -119,4 +115,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-08 14:26:52
+-- Dump completed on 2020-11-08 15:07:31
